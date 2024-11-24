76b7f4239cf9b6f019cda51ea90460ca0a2d46f4
kevinjohannesson <kroon.kevin@gmail.com>
2024-11-24 14:03:58 +0100

    first commit -- nextjs with i18n basic setup

---
+++ middleware.ts
@@ -0,0 +1,107 @@
+// import { NextRequest, NextResponse } from "next/server";
+// // import { match } from "@formatjs/intl-localematcher";
+// // import Negotiator from "negotiator";
+
+// // let defaultLocale = 'en-US'
+// const locales = ["en-US", "nl-NL", "nl"];
+
+// // Get the preferred locale, similar to the above or using a library
+// function getLocale(request: NextRequest) {
+//   const { headers } = request;
+//   console.log({ headers });
+//   // const languages = new Negotiator({ headers }).languages()
+
+//   return "en-US";
+//   // return match(languages, locales, defaultLocale)
+// }
+
+// export function middleware(request: NextRequest) {
+//   console.log("middleware");
+//   console.log(request);
+//   // Check if there is any supported locale in the pathname
+//   const { pathname } = request.nextUrl;
+//   const pathnameHasLocale = locales.some(
+//     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+//   );
+
+//   if (pathnameHasLocale) return;
+
+//   // Redirect if there is no locale
+//   const locale = getLocale(request);
+//   request.nextUrl.pathname = `/${locale}${pathname}`;
+//   // e.g. incoming request is /products
+//   // The new URL is now /en-US/products
+//   return NextResponse.redirect(request.nextUrl);
+// }
+
+// export const config = {
+//   matcher: [
+//     // Skip all internal paths (_next)
+//     "/((?!_next).*)",
+//     // Optional: only run on root (/) URL
+//     // '/'
+//   ],
+// };
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import { i18n } from "./i18n-config";
+
+import { match as matchLocale } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+function getLocale(request: NextRequest): string | undefined {
+  // Negotiator expects plain object so we need to transform headers
+  const negotiatorHeaders: Record<string, string> = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+  const locales = [...i18n.locales];
+
+  // Use negotiator and intl-localematcher to get best locale
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+    locales
+  );
+
+  const locale = matchLocale(languages, locales, i18n.defaultLocale);
+
+  return locale;
+}
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
+  // // If you have one
+  // if (
+  //   [
+  //     '/manifest.json',
+  //     '/favicon.ico',
+  //     // Your other files in `public`
+  //   ].includes(pathname)
+  // )
+  //   return
+
+  // Check if there is any supported locale in the pathname
+  const pathnameIsMissingLocale = i18n.locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(
+      new URL(
+        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
+        request.url
+      )
+    );
+  }
+}
+
+export const config = {
+  // Matcher ignoring `/_next/` and `/api/`
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import { Locale } from "../../../i18n-config";
+import { getDictionary } from "../../../get-dictionary";
 
-export default function Home() {
+export default async function Page(props: {
+  params: Promise<{ lang: Locale }>;
+}) {
+  const { lang } = await props.params;
+
+  const dictionary = await getDictionary(lang);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <p>Current locale: {lang}</p>
+        <p>
+          This text is rendered on the server:{" "}
+          {dictionary["server-component"].welcome}
+        </p>
         <Image
           className="dark:invert"
           src="/next.svg"
